6cf21e3cfd77f83ab102f741bfb23602a5802d79
Raidems <bernardinoerik74@gmail.com>
2025-01-22 14:43:15 -0600

    Se creo la estructura de un archivo java y manejo de candenas.

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -1,10 +1,21 @@
+
+//IMPORTACION.
+
+//Clase principal.
 public class Main {
 
+    //Metodo Main.
     public static void main(String[] args) {
 
-
+        // 2 cadenas con java
         System.out.println("Hello, Erik!");
+        String cadenas = "Erik"+"Narciso"+"Bernardino";
 
-        
+        System.out.println(cadenas);
+        System.out.println(cadenas.substring(2,5));
+        System.out.println(cadenas.substring(2));
+        System.out.println(cadenas.substring(0,5));
+
+
     }
 }
